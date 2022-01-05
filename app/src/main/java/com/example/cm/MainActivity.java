6519c639f4e38ac7a3970057b7122e231859ccff
markusbink <bink-markus@web.de>
2022-01-05 16:42:22 +0100

    #7 Extracted setup of bottom nav bar to own function

--- app/src/main/java/com/example/cm/MainActivity.java
+++ app/src/main/java/com/example/cm/MainActivity.java
@@ -2,8 +2,6 @@ package com.example.cm;
 
 import android.os.Bundle;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -13,7 +11,6 @@ import androidx.navigation.ui.NavigationUI;
 import com.example.cm.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
     private ActivityMainBinding binding;
 
     @Override
@@ -21,11 +18,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+        setContentView(binding.getRoot());
+        setupBottomNavigationBar();
+    }
+
+    private void setupBottomNavigationBar() {
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
@@ -33,5 +31,4 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
-
 }
