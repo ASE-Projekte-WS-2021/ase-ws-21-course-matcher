a5f043141a8b1ea9f6f9dba24041b9a346d4111f
markusbink <bink-markus@web.de>
2022-01-27 16:36:22 +0100

    #59 Added user data to own profile

--- app/src/main/java/com/example/cm/ui/profile/ProfileViewModel.java
+++ app/src/main/java/com/example/cm/ui/profile/ProfileViewModel.java
@@ -1,18 +1,16 @@
 package com.example.cm.ui.profile;
 
-import android.util.Log;
-
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.example.cm.data.models.User;
 import com.example.cm.data.repositories.UserRepository;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.List;
 
 public class ProfileViewModel extends ViewModel implements UserRepository.OnUserRepositoryListener {
 
-    private static final String TAG = "ProfileViewModel";
     private final UserRepository userRepository;
     public MutableLiveData<User> currentUser = new MutableLiveData<>();
 
@@ -24,12 +22,18 @@ public class ProfileViewModel extends ViewModel implements UserRepository.OnUser
         return currentUser;
     }
 
+    public void getLoggedInUser() {
+        FirebaseUser firebaseUser = userRepository.getCurrentUser();
+        userRepository.getUserByEmail(firebaseUser.getEmail());
+    }
+
     public void getUserById(String userId) {
         userRepository.getUserById(userId);
     }
 
     @Override
-    public void onUsersRetrieved(List<User> users) {}
+    public void onUsersRetrieved(List<User> users) {
+    }
 
     @Override
     public void onUserRetrieved(User user) {
