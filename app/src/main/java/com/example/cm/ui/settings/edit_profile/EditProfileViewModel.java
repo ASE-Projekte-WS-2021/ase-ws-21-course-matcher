2e396917f8d5efe79011da064d125f836e2da8a5
markusbink <bink-markus@web.de>
2022-02-25 19:52:46 +0100

    #121 Added business logic to update image

--- app/src/main/java/com/example/cm/ui/settings/edit_profile/EditProfileViewModel.java
+++ app/src/main/java/com/example/cm/ui/settings/edit_profile/EditProfileViewModel.java
@@ -1,6 +1,7 @@
 package com.example.cm.ui.settings.edit_profile;
 
 import android.content.Context;
+import android.net.Uri;
 
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
@@ -10,11 +11,13 @@ import com.example.cm.data.models.Status;
 import com.example.cm.data.models.StatusFlag;
 import com.example.cm.data.models.User;
 import com.example.cm.data.repositories.Callback;
+import com.example.cm.data.repositories.StorageRepository;
 import com.example.cm.data.repositories.UserRepository;
 import com.example.cm.utils.InputValidator;
 import com.google.android.gms.tasks.OnSuccessListener;
 
-public class EditProfileViewModel extends ViewModel implements Callback {
+public class EditProfileViewModel extends ViewModel implements Callback, StorageRepository.Callback {
+    StorageRepository storageRepository = new StorageRepository();
     public MutableLiveData<Status> status = new MutableLiveData<>();
     private UserRepository userRepository;
     private MutableLiveData<User> user;
@@ -28,6 +31,15 @@ public class EditProfileViewModel extends ViewModel implements Callback {
 
     public MutableLiveData<User> getUser() {
         return user;
+    }
+
+
+    public void updateImage(Uri uri) {
+        if(uri == null || user.getValue() == null) {
+            status.postValue(new Status(StatusFlag.ERROR, "Ein Fehler ist aufgetreten"));
+            return;
+        }
+        storageRepository.uploadProfileImage(uri, user.getValue().getId(), this);
     }
 
     public void updateField(String field, String value) {
@@ -76,6 +88,10 @@ public class EditProfileViewModel extends ViewModel implements Callback {
                 break;
             case "Bio":
                 userRepository.updateField("bio", trimmedValue, this);
+                break;
+            case "profileImageUrl":
+                userRepository.updateField("profileImageUrl", trimmedValue, this);
+                break;
             default:
                 break;
         }
@@ -90,6 +106,16 @@ public class EditProfileViewModel extends ViewModel implements Callback {
 
     @Override
     public void onError(Object object) {
+        status.postValue(new Status(StatusFlag.ERROR, "Ein Fehler ist aufgetreten"));
+    }
+
+    @Override
+    public void onSuccess(String url) {
+        updateField("profileImageUrl", url);
+    }
+
+    @Override
+    public void onError(Exception e) {
         status.postValue(new Status(StatusFlag.ERROR, "Ein Fehler ist aufgetreten"));
     }
 }
