e4a87ded0cc5e38a18c69c3c8a7dbccd697bc4e1
markusbink <bink-markus@web.de>
2022-02-25 19:52:29 +0100

    #121 Added user interaction to update image

--- app/src/main/java/com/example/cm/ui/settings/edit_profile/EditProfileFragment.java
+++ app/src/main/java/com/example/cm/ui/settings/edit_profile/EditProfileFragment.java
@@ -1,23 +1,32 @@
 package com.example.cm.ui.settings.edit_profile;
 
+import static android.app.Activity.RESULT_OK;
+
 import android.app.Dialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.cm.R;
 import com.example.cm.config.FieldType;
+import com.example.cm.data.repositories.StorageRepository;
 import com.example.cm.databinding.FragmentEditProfileBinding;
 import com.example.cm.utils.EditTextAreaDialog;
 import com.example.cm.utils.EditTextDialog;
 import com.example.cm.utils.Navigator;
 import com.google.android.material.snackbar.Snackbar;
+import com.squareup.picasso.Picasso;
 
 public class EditProfileFragment extends Fragment implements EditTextDialog.OnSaveListener, EditTextAreaDialog.OnSaveListener {
+    private final int SELECT_IMAGE_REQUEST_CODE = 1;
+
     FragmentEditProfileBinding binding;
     EditProfileViewModel editProfileViewModel;
     Navigator navigator;
@@ -43,6 +52,7 @@ public class EditProfileFragment extends Fragment implements EditTextDialog.OnSa
             binding.inputFirstName.inputField.setText(user.getFirstName());
             binding.inputLastName.inputField.setText(user.getLastName());
             binding.inputFieldBio.setText(user.getBio());
+            Picasso.get().load(user.getProfileImageUrl()).fit().centerCrop().into(binding.profileImage);
         });
 
         editProfileViewModel.status.observe(getViewLifecycleOwner(), status -> {
@@ -52,11 +62,15 @@ public class EditProfileFragment extends Fragment implements EditTextDialog.OnSa
 
             switch (status.getFlag()) {
                 case SUCCESS:
-                    dialog.hide();
+                    if(dialog != null) {
+                        dialog.hide();
+                    }
                     Snackbar.make(binding.getRoot(), status.getMessage(), Snackbar.LENGTH_SHORT).show();
                     break;
                 case ERROR:
-                    dialog.hide();
+                    if(dialog != null) {
+                        dialog.hide();
+                    }
                     Snackbar.make(binding.getRoot(), status.getMessage(), Snackbar.LENGTH_SHORT).show();
                     break;
                 default:
@@ -87,6 +101,28 @@ public class EditProfileFragment extends Fragment implements EditTextDialog.OnSa
         binding.inputFieldBio.setOnClickListener(v -> {
             openDialog(FieldType.TEXT_AREA.toString(), getString(R.string.input_label_bio), binding.inputFieldBio.getText().toString());
         });
+        binding.editProfileImageBtn.setOnClickListener(v -> {
+            onEditProfileImageClicked();
+        });
+    }
+
+
+    private void onEditProfileImageClicked() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_IMAGE_REQUEST_CODE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == SELECT_IMAGE_REQUEST_CODE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri uri = data.getData();
+            binding.profileImage.setImageURI(uri);
+            editProfileViewModel.updateImage(uri);
+        }
     }
 
     private void openDialog(String fieldType, String fieldToUpdate, String valueToEdit) {
