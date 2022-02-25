c7c196bf24b305c9ac0a3f1386844b111fc1659b
markusbink <bink-markus@web.de>
2022-02-25 10:15:21 +0100

    #112 Added viewModel to handle logic

--- app/src/main/java/com/example/cm/ui/settings/edit_profile/EditProfileViewModel.java
+++ app/src/main/java/com/example/cm/ui/settings/edit_profile/EditProfileViewModel.java
@@ -8,9 +8,14 @@ import com.example.cm.data.repositories.Callback;
 import com.example.cm.data.repositories.UserRepository;
 import com.google.android.gms.tasks.OnSuccessListener;
 
-import timber.log.Timber;
+enum Status {
+    SUCCESS,
+    ERROR,
+    LOADING
+}
 
 public class EditProfileViewModel extends ViewModel implements Callback {
+    public MutableLiveData<Status> status = new MutableLiveData<>();
     private UserRepository userRepository;
     private MutableLiveData<User> user;
 
@@ -24,7 +29,12 @@ public class EditProfileViewModel extends ViewModel implements Callback {
     }
 
     public void updateField(String field, String value) {
+        if (value.isEmpty()) {
+            status.postValue(Status.ERROR);
+            return;
+        }
 
+        status.postValue(Status.LOADING);
         switch (field) {
             case "Benutzername":
                 userRepository.updateField("username", value, this);
@@ -35,6 +45,8 @@ public class EditProfileViewModel extends ViewModel implements Callback {
             case "Nachname":
                 userRepository.updateField("lastName", value, this);
                 break;
+            case "Bio":
+                userRepository.updateField("bio", value, this);
             default:
                 break;
         }
@@ -42,12 +54,13 @@ public class EditProfileViewModel extends ViewModel implements Callback {
 
     @Override
     public OnSuccessListener<? super Void> onSuccess(Object object) {
+        status.postValue(Status.SUCCESS);
         user = userRepository.getCurrentUser();
         return null;
     }
 
     @Override
     public void onError(Object object) {
-        Timber.e("onError: %s", object);
+        status.postValue(Status.ERROR);
     }
 }
