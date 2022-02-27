18a315b813f67d0e1cf6c9829aa08e9052ac263d
Martina Emmert <emmert.tin1306@gmail.com>
2022-02-27 20:46:04 +0100

    enhancement/#111 removed unused code

--- app/src/main/java/com/example/cm/ui/invite_friends/InviteFriendsFragment.java
+++ app/src/main/java/com/example/cm/ui/invite_friends/InviteFriendsFragment.java
@@ -1,7 +1,6 @@
 package com.example.cm.ui.invite_friends;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
