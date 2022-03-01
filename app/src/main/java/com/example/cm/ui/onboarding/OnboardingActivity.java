3185097c6ac88d4d3546d6c444c444d357abd055
Nicole Schönwerth <40839111+NiciSc@users.noreply.github.com>
2022-03-01 22:35:39 +0100

    Added onBoarding Activity

---
+++ app/src/main/java/com/example/cm/ui/onboarding/OnboardingActivity.java
@@ -0,0 +1,106 @@
+package com.example.cm.ui.onboarding;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.cm.R;
+import com.example.cm.ui.adapters.OnboardingAdapter;
+import com.example.cm.ui.auth.LoginActivity;
+import com.example.cm.ui.auth.RegisterActivity;
+
+public class OnboardingActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+    LinearLayout dotsLayout;
+    OnboardingAdapter onboardingAdapter;
+    TextView[] dots;
+    Button registrierenBtn;
+    Animation btnAnimation;
+    int currentPos;
+
+    ViewPager.OnPageChangeListener onPageChangeListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            addDots(position);
+            currentPos = position;
+            if (position == 3) {
+                btnAnimation = AnimationUtils.loadAnimation(OnboardingActivity.this, R.anim.bottom_anim);
+                registrierenBtn.setAnimation(btnAnimation);
+                registrierenBtn.setVisibility(View.VISIBLE);
+            } else {
+                registrierenBtn.setVisibility(View.INVISIBLE);
+            }
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding);
+
+        viewPager = findViewById(R.id.onboarding_viewpager);
+        dotsLayout = findViewById(R.id.layout_onboarding_indicator);
+        registrierenBtn = findViewById(R.id.onboarding_btn_register);
+
+        onboardingAdapter = new OnboardingAdapter(this);
+
+        viewPager.setAdapter(onboardingAdapter);
+        addDots(0);
+        viewPager.addOnPageChangeListener(onPageChangeListener);
+    }
+
+    public void skipOB(View view) {
+        startActivity(new Intent(this, LoginActivity.class));
+        finish();
+    }
+
+    public void toRegister(View view) {
+        if (currentPos == 3) {
+            startActivity(new Intent(this, RegisterActivity.class));
+            finish();
+        }
+    }
+
+    private void addDots(int position) {
+        dots = new TextView[4];
+        dotsLayout.removeAllViews();
+        for (int i = 0; i < dots.length; i++) {
+            dots[i] = new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226;"));
+            dots[i].setTextSize(35);
+
+            dotsLayout.addView(dots[i]);
+        }
+        if (dots.length > 0) {
+            dots[position].setTextColor(getResources().getColor(R.color.orange_dark));
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+
+    }
+}
