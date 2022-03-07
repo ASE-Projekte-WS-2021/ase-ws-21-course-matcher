66e139f86b73aa08fe63047beb78e3e560bf673c
markusbink <bink-markus@web.de>
2022-03-07 09:55:38 +0100

    #157 Removed empty lines according to comments

--- app/src/main/java/com/example/cm/ui/settings/SettingsViewModel.java
+++ app/src/main/java/com/example/cm/ui/settings/SettingsViewModel.java
@@ -5,15 +5,13 @@ import androidx.lifecycle.ViewModel;
 import com.example.cm.data.repositories.AuthRepository;
 
 public class SettingsViewModel extends ViewModel {
-    private AuthRepository authRepository;
+    private final AuthRepository authRepository;
 
     public SettingsViewModel() {
         authRepository = new AuthRepository();
     }
 
-
     public void logOut() {
         authRepository.logOut();
     }
-
 }
