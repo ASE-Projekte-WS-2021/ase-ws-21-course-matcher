5106a14bd1bcc62ab61b61b4e8acdfefd744a747
Nicole Schönwerth <40839111+NiciSc@users.noreply.github.com>
2022-03-11 11:25:01 +0100

    Added Constant

--- app/src/main/java/com/example/cm/Constants.java
+++ app/src/main/java/com/example/cm/Constants.java
@@ -19,6 +19,7 @@ public class Constants {
     public static final int TRASH_ICON_MARGIN = 15;
     public static final int TRASH_ICON_SIZE = 175;
     public static final int HALVING_FACTOR = 2;
+    public static final int PASSWORD_MIN = 6;
     public static final LatLng DEFAULT_LOCATION = new LatLng(48.992162698, 12.090332972);
 
 }
