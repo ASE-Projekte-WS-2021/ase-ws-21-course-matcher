ce9b37b06591bea2c2f42f6b536c455712b82127
markusbink <bink-markus@web.de>
2022-03-19 20:04:57 +0100

    #207 Added badges to requests tab
    Indicates whether a user still has requests open

--- app/src/main/java/com/example/cm/ui/friends/FriendsTabsFragment.java
+++ app/src/main/java/com/example/cm/ui/friends/FriendsTabsFragment.java
@@ -1,5 +1,7 @@
 package com.example.cm.ui.friends;
 
+import static com.example.cm.data.models.Request.RequestState.REQUEST_PENDING;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,38 +10,85 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.cm.R;
+import com.example.cm.data.models.FriendRequest;
 import com.example.cm.databinding.FragmentFriendsTabsBinding;
 import com.example.cm.ui.adapters.FriendsTapAdapter;
+import com.example.cm.ui.friends.FriendRequests.FriendRequestsViewModel;
 import com.example.cm.utils.Navigator;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
-public class FriendsTabsFragment extends Fragment {
+import java.util.List;
 
+public class FriendsTabsFragment extends Fragment {
     private FriendsTapAdapter friendsTabAdapter;
+    private FriendRequestsViewModel friendRequestsViewModel;
     private ViewPager2 viewPager;
     private FragmentFriendsTabsBinding binding;
     private Navigator navigator;
+    private int openRequests = 0;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentFriendsTabsBinding.inflate(inflater, container, false);
         navigator = new Navigator(requireActivity());
 
-        binding.addFriendsFab.setOnClickListener(view -> {
-            navigator.navigateToSelectFriends(); //todo: navigate to add friends ???
-        });
+        initViewModel();
+        initListeners();
+
         return binding.getRoot();
+    }
+
+    private void initListeners() {
+        binding.addFriendsFab.setOnClickListener(view -> {
+            navigator.navigateToSelectFriends();
+        });
+    }
+
+    private void initViewModel() {
+        friendRequestsViewModel = new ViewModelProvider(requireActivity()).get(FriendRequestsViewModel.class);
+        friendRequestsViewModel.getFriendRequests().observe(getViewLifecycleOwner(), friendRequests -> {
+            if (friendRequests == null) {
+                return;
+            }
+            countOpenRequests(friendRequests);
+            hideShowBadge();
+        });
+    }
+
+    private void countOpenRequests(List<MutableLiveData<FriendRequest>> friendRequests) {
+        // Reset the open requests counter
+        openRequests = 0;
+
+        for (int i = 0; i < friendRequests.size(); i++) {
+            FriendRequest friendRequest = friendRequests.get(i).getValue();
+            if (friendRequest == null) {
+                continue;
+            }
+            if (friendRequest.getState() == REQUEST_PENDING) {
+                openRequests++;
+            }
+        }
+    }
+
+    private void hideShowBadge() {
+        TabLayout.Tab tab = binding.tabLayout.getTabAt(1);
+        if (tab == null) {
+            return;
+        }
+        if (openRequests > 0) {
+            tab.getOrCreateBadge().setNumber(openRequests);
+            tab.getOrCreateBadge().setBackgroundColor(getResources().getColor(R.color.orange500));
+            tab.getOrCreateBadge().setBadgeTextColor(getResources().getColor(R.color.white));
+            tab.getOrCreateBadge().setVisible(true);
+        } else {
+            tab.getOrCreateBadge().setVisible(false);
+        }
     }
 
     @Override
--- app/src/main/java/com/example/cm/ui/meetup/MeetupTabsFragment.java
+++ app/src/main/java/com/example/cm/ui/meetup/MeetupTabsFragment.java
@@ -1,5 +1,7 @@
 package com.example.cm.ui.meetup;
 
+import static com.example.cm.data.models.Request.RequestState.REQUEST_PENDING;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,21 +10,30 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.cm.R;
+import com.example.cm.data.models.MeetupRequest;
 import com.example.cm.databinding.FragmentMeetupTabsBinding;
 import com.example.cm.ui.adapters.MeetupTabAdapter;
+import com.example.cm.ui.meetup.MeetupRequests.MeetupRequestsViewModel;
 import com.example.cm.utils.Navigator;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
-public class MeetupTabsFragment extends Fragment {
+import java.util.List;
 
+import timber.log.Timber;
+
+public class MeetupTabsFragment extends Fragment {
     private MeetupTabAdapter meetupTabAdapter;
+    private MeetupRequestsViewModel meetupRequestsViewModel;
     private ViewPager2 viewPager;
     private FragmentMeetupTabsBinding binding;
     private Navigator navigator;
+    private int openRequests = 0;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -30,14 +41,59 @@ public class MeetupTabsFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentMeetupTabsBinding.inflate(inflater, container, false);
         navigator = new Navigator(requireActivity());
 
-        binding.addMeetupFab.setOnClickListener(view -> navigator.navigateToCreateMeetup());
+        initViewModel();
+        initListeners();
+
         return binding.getRoot();
+    }
+
+    private void initViewModel() {
+        meetupRequestsViewModel = new ViewModelProvider(requireActivity()).get(MeetupRequestsViewModel.class);
+        meetupRequestsViewModel.getMeetupRequests().observe(getViewLifecycleOwner(), meetupRequests -> {
+            if (meetupRequests == null) {
+                return;
+            }
+            countOpenRequests(meetupRequests);
+            hideShowBadge();
+        });
+    }
+
+    private void countOpenRequests(List<MutableLiveData<MeetupRequest>> meetupRequests) {
+        // Reset the open requests counter
+        openRequests = 0;
+
+        for (int i = 0; i < meetupRequests.size(); i++) {
+            MeetupRequest meetupRequest = meetupRequests.get(i).getValue();
+            if (meetupRequest == null) {
+                continue;
+            }
+            if (meetupRequest.getState() == REQUEST_PENDING) {
+                openRequests++;
+            }
+        }
+    }
+
+    private void hideShowBadge() {
+        TabLayout.Tab tab = binding.tabLayout.getTabAt(1);
+        if (tab == null) {
+            return;
+        }
+        if (openRequests > 0) {
+            tab.getOrCreateBadge().setNumber(openRequests);
+            tab.getOrCreateBadge().setBackgroundColor(getResources().getColor(R.color.orange500));
+            tab.getOrCreateBadge().setBadgeTextColor(getResources().getColor(R.color.white));
+            tab.getOrCreateBadge().setVisible(true);
+        } else {
+            tab.getOrCreateBadge().setVisible(false);
+        }
+    }
+
+    private void initListeners() {
+        binding.addMeetupFab.setOnClickListener(view -> navigator.navigateToCreateMeetup());
     }
 
     @Override
