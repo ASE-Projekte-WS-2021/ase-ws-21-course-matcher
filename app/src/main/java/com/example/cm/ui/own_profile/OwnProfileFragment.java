d92eb25c5495afc5a114f272706f1b72779eaf5f
Nicole Schönwerth <40839111+NiciSc@users.noreply.github.com>
2022-03-23 21:58:25 +0100

    #228 Added Availability Logic

--- app/src/main/java/com/example/cm/ui/own_profile/OwnProfileFragment.java
+++ app/src/main/java/com/example/cm/ui/own_profile/OwnProfileFragment.java
@@ -1,21 +1,35 @@
 package com.example.cm.ui.own_profile;
 
+import static com.example.cm.data.models.Availability.AVAILABLE;
+import static com.example.cm.data.models.Availability.SOON_AVAILABLE;
+import static com.example.cm.data.models.Availability.UNAVAILABLE;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.PopupMenu;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.cm.Constants;
 import com.example.cm.R;
+import com.example.cm.data.listener.UserListener;
 import com.example.cm.data.models.Availability;
 import com.example.cm.data.models.User;
 import com.example.cm.databinding.FragmentOwnProfileBinding;
 import com.example.cm.utils.Navigator;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.material.snackbar.Snackbar;
 import com.squareup.picasso.Picasso;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import timber.log.Timber;
 
 public class OwnProfileFragment extends Fragment {
 
@@ -24,9 +38,11 @@ public class OwnProfileFragment extends Fragment {
     private Navigator navigator;
     private GoogleMap map;
     private Availability availability;
+    private PopupMenu popup;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentOwnProfileBinding.inflate(inflater, container, false);
+        initAvailabilityMenu();
         initViewModel();
         initListeners();
 
@@ -40,6 +56,10 @@ public class OwnProfileFragment extends Fragment {
         });
         binding.btnEditProfile.setOnClickListener(v -> {
             navigator.getNavController().navigate(R.id.action_ownProfileFragment_to_editProfileFragment);
+        });
+
+        binding.dotAvailabilityIcon.setOnClickListener(v ->{
+            popup.show();
         });
     }
 
@@ -55,22 +75,23 @@ public class OwnProfileFragment extends Fragment {
             binding.tvBioDescription.setText(currentUser.getBio());
             if (currentUser.getProfileImageUrl() != null && !currentUser.getProfileImageUrl().isEmpty()) {
                 binding.ivProfileImage.setImageTintMode(null);
-                binding.ivProfileImage.setScaleX(1f);
-                binding.ivProfileImage.setScaleY(1f);
+                binding.ivProfileImage.setScaleX(Constants.FINAL_CARD_ALPHA);
+                binding.ivProfileImage.setScaleY(Constants.FINAL_CARD_ALPHA);
                 Picasso.get().load(currentUser.getProfileImageUrl()).fit().centerCrop().into(binding.ivProfileImage);
                 binding.btnProfileSettings.bringToFront();
             }
+
             availability = currentUser.getAvailability();
             if (availability != null) {
                 switch (availability) {
                     case AVAILABLE:
-                        binding.dotAvailabilityIcon.setImageResource(R.drawable.ic_dot_available);
+                        binding.dotAvailabilityIcon.setImageResource(R.drawable.ic_available_button);
                         break;
                     case SOON_AVAILABLE:
-                        binding.dotAvailabilityIcon.setImageResource(R.drawable.ic_dot_soon_available);
+                        binding.dotAvailabilityIcon.setImageResource(R.drawable.ic_soon_button);
                         break;
                     case UNAVAILABLE:
-                        binding.dotAvailabilityIcon.setImageResource(R.drawable.ic_dot_unavailable);
+                        binding.dotAvailabilityIcon.setImageResource(R.drawable.ic_unavailable_button);
                         break;
                 }
             }
@@ -91,6 +112,102 @@ public class OwnProfileFragment extends Fragment {
             Picasso.get().load(currentUser.getProfileImageUrl()).fit().centerCrop().into(binding.ivProfileImage);
         }
         binding.btnProfileSettings.bringToFront();
+    }
+
+
+    private void initAvailabilityMenu() {
+        popup = new PopupMenu(requireContext(), binding.dotAvailabilityIcon);
+        setForceShowIcon(popup);
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.menu_availability_state, popup.getMenu());
+
+        popup.setOnMenuItemClickListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.menuAvailable:
+                    onAvailable();
+                    break;
+                case R.id.menuSoonAvailable:
+                    onSoonAvailable();
+                    break;
+                case R.id.menuUnavailable:
+                    onUnavailable();
+                    break;
+            }
+            return true;
+        });
+    }
+
+    // https://stackoverflow.com/questions/20836385/popup-menu-with-icon-on-android
+    private void setForceShowIcon(PopupMenu popupMenu) {
+        try {
+            Field[] mFields = popupMenu.getClass().getDeclaredFields();
+            for (Field field : mFields) {
+                if ("mPopup".equals(field.getName())) {
+                    field.setAccessible(true);
+                    Object menuPopupHelper = field.get(popupMenu);
+                    Class<?> popupHelper = Class.forName(menuPopupHelper.getClass().getName());
+                    Method mMethods = popupHelper.getMethod("setForceShowIcon", boolean.class);
+                    mMethods.invoke(menuPopupHelper, true);
+                    break;
+                }
+            }
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void onSoonAvailable() {
+        ownProfileViewModel.updateAvailablilty(SOON_AVAILABLE, new UserListener<Availability>() {
+            @Override
+            public void onUserSuccess(Availability availability) {
+                setSoonAvailableUI();
+            }
+
+            @Override
+            public void onUserError(Exception error) {
+                Snackbar.make(binding.getRoot(), R.string.availavilityError, Snackbar.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void onUnavailable() {
+        ownProfileViewModel.updateAvailablilty(UNAVAILABLE, new UserListener<Availability>() {
+            @Override
+            public void onUserSuccess(Availability availability) {
+                setUnavailableUI();
+            }
+
+            @Override
+            public void onUserError(Exception error) {
+                Snackbar.make(binding.getRoot(), R.string.availavilityError, Snackbar.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void onAvailable() {
+        ownProfileViewModel.updateAvailablilty(AVAILABLE, new UserListener<Availability>() {
+            @Override
+            public void onUserSuccess(Availability availability) {
+                setAvailableUI();
+            }
+
+            @Override
+            public void onUserError(Exception error) {
+                Snackbar.make(binding.getRoot(), R.string.availavilityError, Snackbar.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void setAvailableUI() {
+        binding.dotAvailabilityIcon.setImageResource(R.drawable.ic_available_button);
+    }
+
+    private void setUnavailableUI() {
+        binding.dotAvailabilityIcon.setImageResource(R.drawable.ic_unavailable_button);
+    }
+
+    private void setSoonAvailableUI() {
+        binding.dotAvailabilityIcon.setImageResource(R.drawable.ic_soon_button);
     }
 
 
--- app/src/main/java/com/example/cm/ui/own_profile/OwnProfileViewModel.java
+++ app/src/main/java/com/example/cm/ui/own_profile/OwnProfileViewModel.java
@@ -3,8 +3,12 @@ package com.example.cm.ui.own_profile;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.cm.data.listener.UserListener;
+import com.example.cm.data.models.Availability;
 import com.example.cm.data.models.User;
+import com.example.cm.data.repositories.Callback;
 import com.example.cm.data.repositories.UserRepository;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class OwnProfileViewModel extends ViewModel {
 
@@ -18,5 +22,20 @@ public class OwnProfileViewModel extends ViewModel {
 
     public MutableLiveData<User> getCurrentUser() {
         return currentUser;
+    }
+
+    public void updateAvailablilty(Availability availabilityState, UserListener<Availability> listener) {
+        userRepository.updateField("availability", availabilityState, new Callback() {
+            @Override
+            public OnSuccessListener<? super Void> onSuccess(Object object) {
+                listener.onUserSuccess(availabilityState);
+                return null;
+            }
+
+            @Override
+            public void onError(Object object) {
+                listener.onUserError((Exception) object);
+            }
+        });
     }
 }
