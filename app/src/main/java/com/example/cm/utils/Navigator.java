13db16cc2a6c1de6be2984dd02b4f30ca7351ed5
markusbink <bink-markus@web.de>
2022-04-02 12:00:04 +0200

    #279 Removed unused methods

--- app/src/main/java/com/example/cm/utils/Navigator.java
+++ app/src/main/java/com/example/cm/utils/Navigator.java
@@ -7,8 +7,7 @@ import com.example.cm.R;
 
 public class Navigator {
 
-    NavController navController;
-    // TODO hier alle navigator rein damit übersichtlicher & einheitlicher
+    private NavController navController;
 
     public Navigator(FragmentActivity activity) {
         this.navController = Utils.findNavController(activity);
@@ -18,16 +17,8 @@ public class Navigator {
         navController.navigate(R.id.navigateToSelectFriends);
     }
 
-    public void navigateToInviteFriends() {
-        navController.navigate(R.id.navigateToInviteFriends);
-    }
-
     public void navigateToCreateMeetup() {
         navController.navigate(R.id.navigateToMeetups);
-    }
-
-    public void globalNavigateToProfile() {
-        navController.navigate(R.id.fromFriendsToProfile);
     }
 
     public NavController getNavController() {
