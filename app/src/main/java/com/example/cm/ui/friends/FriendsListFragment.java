615e57370e452d97fe0414dad957de0ef64d0a31
markusbink <bink-markus@web.de>
2022-04-04 20:48:08 +0200

    #276 Removed unnecessary onResume
    Not needed since it re-set the initial list which resulted in the filtering not showing properly

--- app/src/main/java/com/example/cm/ui/friends/FriendsListFragment.java
+++ app/src/main/java/com/example/cm/ui/friends/FriendsListFragment.java
@@ -121,15 +121,19 @@ public class FriendsListFragment extends Fragment implements OnItemClickListener
     }
 
     private void updateListByQuery(String query) {
+        Timber.d("updateListByQuery: %s", query);
         List<User> filteredUsers = friendsViewModel.getFilteredUsers(query);
         if (filteredUsers == null) {
+            Timber.d("updateListByQuery: filteredUsers is null");
             return;
         }
         if (filteredUsers.isEmpty()) {
+            Timber.d("updateListByQuery: filteredUsers is empty");
             binding.noFriendsWrapper.setVisibility(View.VISIBLE);
             binding.rvUserList.setVisibility(View.GONE);
             return;
         }
+        Timber.d("updateListByQuery: filteredUsers is not empty");
         binding.noFriendsWrapper.setVisibility(View.GONE);
         binding.rvUserList.setVisibility(View.VISIBLE);
         friendsListAdapter.setFriends(filteredUsers);
@@ -146,13 +150,5 @@ public class FriendsListFragment extends Fragment implements OnItemClickListener
         Bundle bundle = new Bundle();
         bundle.putString(Constants.KEY_USER_ID, id);
         navigator.getNavController().navigate(R.id.fromFriendsToProfile, bundle);
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        if (friendsViewModel != null) {
-            observeFriends();
-        }
     }
 }
